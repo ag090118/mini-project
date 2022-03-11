8bfbcac64eea3c82fcad907e55fd07c2f7a72931
Smitesh25 <iit2019090@iiita.ac.in>
2022-03-11 17:31:02 +0530

    added user info card

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -3,47 +3,21 @@ import React from "react";
 function PersonalInfo() {
   return (
     <div>
-    {/* <div class="container">
-  <div class="grid-7 element-animation">
-    <div class="card color-card">
-      <ul>
-        <li><i class="fas fa-arrow-left i-l w"></i></li>
-        <li><i class="fas fa-ellipsis-v i-r w"></i></li>
-        <li><i class="far fa-heart i-r w"></i></li>
-      </ul>
-      <img src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7d5363c18112a02ce22d0c46f8570147&auto=format&fit=crop&w=635&q=80%20635w" alt="profile-pic" class="profile"/>
-      <h1 class="title">Bevely Little</h1>
-      <p class="job-title"> SENIOR PRODUCT DESIGNER</p>
-      <div class="desc top">
-        <p>Create usable interface and designs @GraphicSpark</p>
+        <div class="container">
+      <div class="cover-photo">
+        <img src="https://images.unsplash.com/photo-1565464027194-7957a2295fb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" class="profile"/>
       </div>
-      <button class="btn color-a top"> Hire me</button>
-
-      <hr/>
-      <div class="container">
-        <div class="content">
-          <div class="grid-2">
-            <button class="color-b circule"> <i class="fab fa-dribbble fa-2x"></i></button>
-            <h2 class="title">12.3k</h2>
-            <p class="followers">Followers</p>
-          </div>
-          <div class="grid-2">
-            <button class="color-c circule"><i class="fab fa-behance fa-2x"></i></button>
-            <h2 class="title">16k</h2>
-            <p class="followers">Followers</p>
-          </div>
-          <div class="grid-2">
-            <button class="color-d circule"><i class="fab fa-github-alt fa-2x"></i></button>
-            <h2 class="title">17.8k</h2>
-            <p class="followers">Followers</p>
-          </div>
-        </div>
+      <div class="profile-name">Beni Smith</div>
+      <p class="about">User Interface Designer and<br/>front-end developer</p>
+      <button class="msg-btn">Message</button>
+      <button class="follow-btn">Following</button>
+      <div>
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-youtube"></i>
+        <i class="fab fa-twitter"></i>
       </div>
-    </div>
-  </div>
-  </div> */}
-
-      <h1>FIRST</h1>
+      </div>
     </div>
   );
 }
